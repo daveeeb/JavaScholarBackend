738c9f93376f4c91af227129b71b300c97230601
David <dave.brz32@gmail.com>
2025-10-07 22:33:10 -0600

    feat: adding code

--- src/main/java/mvc/view/AuthorView.java
+++ src/main/java/mvc/view/AuthorView.java
@@ -1,7 +1,15 @@
 package mvc.view;
 
+import mvc.model.Article;
 import mvc.model.Author;
 import mvc.model.Interest;
+import java.util.List;
+
+/**
+ * @author Javier David Barraza Ureña ID 3303
+ * The AuthorView class is responsible for displaying author and article information
+ * to the user in a formatted and readable way through the console.
+ */
 
 public class AuthorView {
     public void displayAuthorDetails(Author author) {
@@ -23,6 +31,26 @@ public class AuthorView {
         }
         System.out.println("Thumbnail: " + author.thumbnail());
     }
+    /**
+     * Displays a list of the author’s published articles.
+     *
+     * @param articles The list of articles written by the author.
+     */
+        public void displayArticles(List<Article> articles) {
+            System.out.println("----------------------------------------");
+            System.out.println("          Articles by Author            ");
+            System.out.println("----------------------------------------");
+
+            for (Article article : articles) {
+                System.out.println("Title: " + article.title());
+                System.out.println("Authors: " + article.authors());
+                System.out.println("Publication Date: " + article.publicationDate());
+                System.out.println("Link: " + article.link());
+                System.out.println("Cited by: " + article.citedBy());
+                System.out.println("Keywords: " + article.keywords());
+                System.out.println("----------------------------------------");
+            }
+        }
     public void displayError(String message) {
         System.err.println(" ERROR: " + message);
     }
