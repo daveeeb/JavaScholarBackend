26588172a5ca9c0bc853786d412869ebe00dd426
David <dave.brz32@gmail.com>
2025-10-07 22:20:59 -0600

    feat: Update the cAuthorController.java and Author.java

--- src/main/java/mvc/model/Author.java
+++ src/main/java/mvc/model/Author.java
@@ -2,6 +2,12 @@ package mvc.model;
 
 import java.util.List;
 
+     /**
+     * Represents an academic author retrieved from Google Scholar.
+     * Each author object stores metadata about a publication such as
+     * name, affiliations, email, interests, thumbnail.
+     */
+
 public record Author(String name, String affiliations, String email, List<Interest> interests, String thumbnail) {
 }
 
